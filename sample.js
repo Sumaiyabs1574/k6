acdcee86ff21b024ef95116c556d29875454c655
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-05-14 15:08:38 +0600

    show iteration per id

--- sample.js
+++ sample.js
@@ -1,28 +1,30 @@
 import http from "k6/http";
 import { check, sleep } from "k6";
+import exec from  "k6/execution";
+export const user =JSON.parse(open("data.json"))
 export const options = {
-  Senarios:{
-    accountCreate:{
-      executer:"per_vus_iterations",
-  vus: 5,
-  iterations:10,
-  duration: "1m",
-},
-accountCreate2:{
-  executer:"Shared_iterations",
-vus: 5,
-iterations:10,
-duration: "1m",
-},
+  //scenarios:{
+    //accountCreate:{
+    //  executer:"per_vus_iterations",
+  //vus: 5,
+ // iterations:10,
+  //duration: "1m",
+//},
+scenarios: {
+  contacts: {
+    executor: 'shared-iterations',
+    vus: 10,
+    iterations: 200,
 
 
-}};
+}}};
 export default function () {
-  const baseUrl = "https://reqres.in/";
-  const endPoint = "api/users/2";
-  const res = http.get(`${baseUrl}${endPoint}`);
-  check(res, { "is status 200": (r) => r.status === 200 });
-  res.body;
+  console.log(exec.vu.idInTest);
+ // const baseUrl = "https://reqres.in/";
+ // const endPoint = "api/users/2";
+  //const res = http.get(`${baseUrl}${endPoint}`);
+  //check(res, { "is status 200": (r) => r.status === 200 });
+ // res.body;
    // console.log(res);
   sleep(1);
 }
