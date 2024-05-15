9f45a98b5c9e944be427be511760a6332a593185
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-05-15 10:32:35 +0600

    threshol

--- sample.js
+++ sample.js
@@ -1,50 +1,13 @@
-import {htmlReport} from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
-import {textSummary} from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
-import {check, sleep} from 'k6';
-import http from "k6/http";
-import {envData} from "../utils/file_helper.js";
+import http from 'k6/http';
 
 export const options = {
-    thresholds: {
-        "http_req_duration": [{threshold: 'p(90)<35', abortOnFail: true, delayAbortEval: '10s'}]
-    },
-    scenarios: {
-        accountCreate: {
-            executor: "per-vu-iterations",
-            vus: 5,
-            iterations: 10
-        },
-        accountCreate2: {
-            executor: "shared-iterations",
-            vus: 10,
-            iterations: 10
-        },
-        accountCreate3: {
-            executor: "constant-vus",
-            vus: 10,
-            duration: '10s'
-        },
-        accountCreate4: {
-            executor: "ramping-vus",
-            startVUs: 0,
-            stages: [
-                {duration: '20s', target: 100},
-                {duration: '30s', target: 100},
-                {duration: '0s', target: 0},
-            ],
-            gracefulRampDown: '30s'
-        }
-    }
-}
-export default function () {
-    const res = http.get(`${envData.baseUrl}/api/users/2`);
-    check(res, {'is status 200': (r) => r.status === 200});
-    sleep(1);
-}
+  vus: 30,
+  duration: '2m',
+  thresholds: {
+    http_req_duration: [{ threshold: 'p(99) < 10', abortOnFail: true }],
+  },
+};
 
-export function handleSummary(data) {
-    return {
-        "summary.html": htmlReport(data),
-        stdout: textSummary(data, {indent: " ", enableColors: true}),
-    };
+export default function () {
+  http.get('https://test-api.k6.io/public/crocodiles/1/');
 }
