0f53710473ca9c8b3107724c247fa59ae04e860f
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-05-15 14:41:14 +0600

    create HTML report

--- matrics.js
+++ matrics.js
@@ -1,3 +1,5 @@
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 import http from 'k6/http';
 import { Trend } from 'k6/metrics';
 
@@ -8,3 +10,8 @@ export default function () {
   myTrend.add(r.timings.waiting);
   console.log(myTrend.name); 
 }
+export function handleSummary(data) {
+    return {
+        "summary.html": htmlReport(data),
+        stdout: textSummary(data, {indent: " ", enableColors: true}),
+        };}
--- sample.js
+++ sample.js
@@ -1,5 +1,6 @@
 import http from 'k6/http';
-
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+import { textSummary } from "https://jslib.k6.io/k6-summary/0.0.1/index.js";
 export const options = {
   vus: 30,
   duration: '2m',
@@ -10,4 +11,10 @@ export const options = {
 
 export default function () {
   http.get('https://test-api.k6.io/public/crocodiles/1/');
+}
+export function teardown(data) {
+  return {
+    "summary.html": htmlReport(data),
+    stdout: textSummary(data, { indent: " ", enableColors: true }),
+  };
 }
