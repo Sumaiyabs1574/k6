dbcc6aef7573bd6d2c316bb90784d982d5714465
Sumaiya Habib <sumaiya.habib@brainstation-23.com>
2024-05-16 17:45:49 +0600

    smoketest

---
+++ smoke.js
@@ -0,0 +1,13 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+  vus: 3, 
+  duration: '30s', 
+};
+
+export default () => {
+  const urlRes = http.get('https://test-api.k6.io');
+  sleep(1);
+  
+};
